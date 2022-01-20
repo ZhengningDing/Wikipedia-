4b755656cb51bb8017be0c74345d1d8368a73c9a
ZhengningDing <z.ding@students.uu.nl>
2022-01-20 15:57:11 +0100

    second commit

--- Tamar.r
+++ Tamar.r
@@ -13,10 +13,25 @@ data_0_1 <- data %>%
   filter(Birth >= 1922 & Birth < 2022) %>%
   mutate(Age = 2022 - as.numeric(Birth))
 
+
+'''
+variables=["Height","Weight","Eyecolor","Haircolor","Hipsize"]
+
+data_0_1 <- data %>%
+  repeat{
+    mutate()
+  }
+  mutate(selected_variable=! is.na(selected_variable))%>%
+  filter(Age>=0 & Age <=100)%>%
+  mutate(Age=2022-as.numeric(Birth))
+  '''
+
+
+
 # proportion of weight entries
 weight <- data_0_1 %>%
   group_by(Age,Gender)%>%
-  summarise(Proportion_Weight=mean(Weight))%>%
+  summarise(weight=mean(Weight))%>%
   ungroup()
 
 # proportion of height entries
@@ -40,9 +55,28 @@ hipsize <- data_0_1 %>%
   summarise(hipsize = mean(HipSize))%>%
   ungroup()
 
-merged<-cbind(weight,height,haircolor,eyecolor,hipsize)
+
+merged <- merge(merge(merge(
+  weight,
+  height,by=c("Age","Gender")),
+  eyecolor,by=c("Age","Gender")),
+  haircolor,by=c("Age","Gender"))
+
 
 # Visualisations 
+#Aggregate
+merged%>%
+  pivot_longer(c(3:6),names_to = "variable",values_to = "proportion")%>%
+  ggplot(aes(x=Age,y=proportion,color=Gender))+
+  facet_wrap(vars(variable),ncol=2)+
+  geom_point()+
+  theme_light()+
+  labs(x= "Age" ,y="Proportion of certain traits mentioned in the age group")
+
+
+
+
+'''
 # weight
 ggplot(data = weight) +
   aes(x = Age, y = weight, color = Gender) +
